79a2a56e45ee30d8cb39e94716c66f53c036b6ea
Mia <crazyanz888@gmail.com>
2019-01-09 16:16:51 +1300

    date picker

--- src/js/component/TraverseContent.js
+++ src/js/component/TraverseContent.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react';
+import DatePickerComponent from './DatePickerComponent';
 
 export default class TraverseContent extends Component{
     render(){
         return(
             <div>
-                Traverse Content
+                
+                <DatePickerComponent />
             </div>
         )
     }
