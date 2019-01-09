7354b0487b66e557b85855b5db7646415ce2b6e1
Mia <crazyanz888@gmail.com>
2019-01-09 16:01:14 +1300

    content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './js/component/Header';
+import TraverseContent from './js/component/TraverseContent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <TraverseContent />
       </div>
     );
   }
---
+++ src/js/component/TraverseContent.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export default class TraverseContent extends Component{
+    render(){
+        return(
+            <div>
+                Traverse Content
+            </div>
+        )
+    }
+}
